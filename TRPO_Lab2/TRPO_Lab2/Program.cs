41dce3b23117133003e9385be6ff16206746483d
ChebrovEM <eliseyyar@yandex.ru>
2020-11-25 17:28:36 +0300

    Fix dividing by zero

--- TRPO_Lab2/TRPO_Lab2/Program.cs
+++ TRPO_Lab2/TRPO_Lab2/Program.cs
@@ -25,6 +25,7 @@ namespace TRPO_Lab2
                     res = o1 * o2;
                     break;
                 case "/":
+                    if (o2 == 0) { Console.WriteLine("На 0 делить нельзя!"); return; }
                     res = o1 / o2;
                     break;
             }
