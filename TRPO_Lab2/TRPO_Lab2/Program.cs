cce31705aca4131cab8204f76fbf76694b68e06b
ChebrovEM <eliseyyar@yandex.ru>
2020-11-25 17:07:24 +0300

    Calc

--- TRPO_Lab2/TRPO_Lab2/Program.cs
+++ TRPO_Lab2/TRPO_Lab2/Program.cs
@@ -6,7 +6,29 @@ namespace TRPO_Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите первый операнд");
+            double o1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите знак операции");
+            string op = Console.ReadLine();
+            Console.WriteLine("Введите второй операнд");
+            double o2 = Convert.ToDouble(Console.ReadLine());
+            double res = 0;
+            switch (op)
+            {
+                case "+":
+                    res = o1 + o2;
+                    break;
+                case "-":
+                    res = o1 - o2;
+                    break;
+                case "*":
+                    res = o1 * o2;
+                    break;
+                case "/":
+                    res = o1 / o2;
+                    break;
+            }
+            Console.WriteLine($"{o1}{op}{o2}={res}");
         }
     }
 }
