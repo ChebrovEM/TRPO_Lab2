7c4d8a4f269f73fed440b0e80e99f7c04f639e5e
ChebrovEM <eliseyyar@yandex.ru>
2020-11-25 17:49:42 +0300

    Fix wrong symbol in operator

--- TRPO_Lab2/TRPO_Lab2/Program.cs
+++ TRPO_Lab2/TRPO_Lab2/Program.cs
@@ -10,6 +10,7 @@ namespace TRPO_Lab2
             double o1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите знак операции");
             string op = Console.ReadLine();
+            if (op != "+" && op != "-" && op != "*" && op != "/") { Console.WriteLine("Неверный оператор!"); return; }
             Console.WriteLine("Введите второй операнд");
             double o2 = Convert.ToDouble(Console.ReadLine());
             double res = 0;
@@ -25,7 +26,6 @@ namespace TRPO_Lab2
                     res = o1 * o2;
                     break;
                 case "/":
-                    if (o2 == 0) { Console.WriteLine("На 0 делить нельзя!"); return; }
                     res = o1 / o2;
                     break;
             }
